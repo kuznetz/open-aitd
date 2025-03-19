6a36c06a45100f10b7aab8689fad0bd7f8f1cdf9
Georgiy Kuznecov <georgiy.kuznecov@gmail.com>
2025-03-19 04:00:32 +0300

    life parser

--- src/extractor/life_extractor.cpp
+++ src/extractor/life_extractor.cpp
@@ -7,33 +7,191 @@
 
 using namespace std;
 
-typedef enum LifeOpParam {
-	var,
-	cvar,
-	animation,
-	sound,
-	model,
-	actor
-} LifeOpParam;
+typedef enum LifeOpArgType {
+	eConst,
+	eVar,
+	eAddr,
+	eActor,
+} LifeOpArg;
 
-typedef struct LifeOp {
+typedef struct LifeOpCode {
 	u8 code;
 	string name;
-	vector<u8> args;
+	vector<LifeOpArgType> args;
+} LifeOpCode;
+
+typedef enum LifeEvalType
+{
+	NONE = -1,
+	ACTOR_COLLIDER,
+	TRIGGER_COLLIDER,
+	HARD_COLLIDER,
+	HIT,
+	HIT_BY,
+	ANIM,
+	END_ANIM,
+	FRAME,
+	END_FRAME,
+	BODY,
+	MARK,
+	NUM_TRACK,
+	CHRONO,
+	ROOM_CHRONO,
+	DIST,
+	COL_BY,
+	ISFOUND,
+	ACTION,
+	POSREL,
+	KEYBOARD_INPUT,
+	SPACE,
+	CONTACT,
+	ALPHA,
+	BETA,
+	GAMMA,
+	INHAND,
+	HITFORCE,
+	CAMERA,
+	RAND,
+	FALLING,
+	ROOM,
+	LIFE,
+	OBJECT,
+	ROOMY,
+	TEST_ZV_END_ANIM,
+	MUSIC,
+	C_VAR,
+	STAGE,
+	THROW,
+	MATRIX,
+	HARD_MAT,
+	PROTECT,
+	END_WAY,
+	DIV_BY_2,
+	UNKNOWN
+} LifeEvalType;
+
+typedef struct LifeOperVar {
+	LifeEvalType type;
+	s16 value;
 } LifeOp;
 
-const vector<LifeOp> lifeOps = { 
+typedef struct LifeOper {
+	const LifeOpCode* opCode = 0;
+	vector<LifeOperVar> vars;
+	vector<s16> consts;
+	s16 egoto;
+} LifeOp;
+
+
+const vector<LifeOpCode> lifeOpCodes = {
 	{ 0, "DO_MOVE", {} },
-	{ 0, "ANIM_ONCE", { animation, animation } },
-	{ 0, "ANIM_ALL_ONCE", { animation, animation } }
+	{ 1, "ANIM_ONCE", { eVar, eVar } },
+	{ 2, "ANIM_ALL_ONCE", { eVar, eVar } },
+	{ 5, "IF_DIFFERENT", { eVar, eVar, eAddr } },
 };
+
+void evalVar(u8*& datap) {
+	u16 evarTypeN = READ_LE_S16(datap);
+	datap += 2;
+	//const
+	if (evarTypeN == -1) {
+		u16 c = READ_LE_S16(datap);
+		datap += 2;
+		return;
+	}
+	if (evarTypeN == 0) {
+		//variable
+		u16 c = READ_LE_S16(datap);
+		datap += 2;
+		return;
+	}
+}
+
+LifeOper readOperation(u8* &datap) {
+	LifeOper result;
+	u16 opCodeN = READ_LE_U16(datap);
+	datap += 2;
+	for (int i = 0; i < lifeOpCodes.size(); i++)
+	{
+		if (lifeOpCodes[i].code == opCodeN) {
+			result.opCode = &lifeOpCodes[i];
+			break;
+		}
+	}
+	if (result.opCode == 0) throw new exception("Opcode not found");
+
+	for (int i = 0; i < result.opCode->args.size(); i++)
+	{
+		switch (result.opCode->args[i]) {
+		case eVar:
+			evalVar(datap);
+			break;
+
+		case eConst:
+		case eAddr:
+		case eActor:
+			auto a = result.opCode->args[i];
+			s16 arg = READ_LE_S16(datap);
+			datap += 2;
+			break;
+		}
+	}
+
+	return result;
+}
 
 void extractLife(string fname, string outDir)
 {
 	PakFile pak(fname);
-	//for (int i = 0; i < 0; i++) {
-	//
-	//}
+	int i = 2;
+	//for (int i = 0; i < pak.headers.size(); i++)
+	{
+		auto& data = pak.readBlock(i);
+		auto datap = data.data();
+		auto datap_end = datap + pak.headers[i].uncompressedSize;
+
+		while (datap < datap_end) {
+			auto& oper = readOperation(datap);
+		}
+		
+
+/*
+	while (pos < allBytes.Length)
+	{
+		int position = pos;
+
+		int curr = allBytes.ReadShort(pos);
+		int eActor = -1;
+		if ((curr & 0x8000) == 0x8000)
+		{
+			curr &= 0x7FFF;
+			pos += 2;
+			eActor = allBytes.ReadShort(pos);
+		}
+
+		if (curr < 0 || curr >= config.LifeMacro.Length)
+		{
+			throw new IndexOutOfRangeException(curr.ToString());
+		}
+		LifeEnum life = config.LifeMacro[curr];
+
+		Instruction ins = new Instruction
+		{
+			Type = life
+		};
+
+		if (eActor != -1) ins.Actor = GetObjectName(eActor);
+		pos += 2;
+
+		ParseArguments(life, ins);
+		var node = nodes.AddLast(ins);
+		nodesMap.Add(position, node);
+		ins.Position = position;
+		ins.Size = pos - position;
+	}
+*/
+
+	}
 }
 
 /*{
