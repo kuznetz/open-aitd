f598badb41b95239b532ac9775db41df87a9f7ab
Georgiy Kuznecov <georgiy.kuznecov@gmail.com>
2025-03-19 16:12:28 +0300

    Life instruction parser (particly)

---
+++ src/extractor/life/life_extractor.cpp
@@ -0,0 +1,99 @@
+﻿#include "../structs/int_types.h"
+#include "../pak/pak.h"
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include "./life_v1.h"
+
+using namespace std;
+
+s16 read16(u8*& datap) {
+	s16 res = READ_LE_S16(datap);
+	datap += 2;
+	return res;
+}
+
+LifeExpr readExpr(u8*& datap) {
+	LifeExpr result;
+	s16 varTypeN = read16(datap);
+	if (varTypeN == -1) {
+		result.constVal = read16(datap);
+		return result;
+	}
+
+	auto& parse = EvalTable_v1[varTypeN];
+	result.Type = parse.Type;
+
+	for (int i = 0; i < parse.arguments.size(); i++)
+	{
+		LifeExpr arg;
+		switch (parse.arguments[i]) {
+		case lifeConst:
+			arg.constVal = read16(datap);
+			result.arguments.push_back(arg);
+			break;
+		case lifeExpr:
+			arg = readExpr(datap);
+			result.arguments.push_back(arg);
+			break;
+		}
+	}
+
+	return result;
+}
+
+
+LifeInstruction readInstruction(u8* &datap) {
+	LifeInstruction result;
+	u16 opCodeN = read16(datap);
+	auto& parse = LifeTable_v1[opCodeN];	
+	result.Type = parse.Type;
+	//if (result.opCode == 0) throw new exception("Opcode not found");
+
+	//if ((varTypeN & 0x8000) == 0x8000)
+	//{
+	//	//change actor
+	//	result.Actor = read16(datap);
+	//}
+	//varTypeN &= 0x7FFF;
+
+	for (int i = 0; i < parse.arguments.size(); i++)
+	{
+		LifeExpr arg;
+		switch (parse.arguments[i]) {
+			case lifeConst:
+				arg.constVal = read16(datap);
+				result.arguments.push_back(arg);
+				break;
+			case lifeGoto:
+				result.Goto = read16(datap);
+				break;
+			case lifeExpr:
+				arg = readExpr(datap);
+				result.arguments.push_back(arg);
+				break;
+		}
+	}
+	return result;
+}
+
+void extractLife(string fname, string outDir)
+{
+	PakFile pak(fname);
+	int i = 6;
+	//for (int i = 0; i < pak.headers.size(); i++)
+	{
+		auto& data = pak.readBlock(i);
+		auto datap = data.data();
+		auto datap_end = datap + pak.headers[i].uncompressedSize;
+
+		vector<LifeInstruction> life;
+		while (true) {
+			auto& oper = readInstruction(datap);
+			life.push_back(oper);
+			if (oper.Type == LifeEnum::ENDLIFE) break;
+			if (datap >= datap_end) throw new exception("No ENDLIFE");
+		}
+	}
+}
---
+++ src/extractor/life/life_structs.h
@@ -0,0 +1,230 @@
+﻿#pragma once
+#include "../structs/int_types.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace EvalEnum {
+	enum EvalEnum
+	{
+		CONST,
+		VAR,
+		ACTOR_COLLIDER,
+		TRIGGER_COLLIDER,
+		HARD_COLLIDER,
+		HIT,
+		HIT_BY,
+		ANIM,
+		END_ANIM,
+		FRAME,
+		END_FRAME,
+		BODY,
+		MARK,
+		NUM_TRACK,
+		CHRONO,
+		ROOM_CHRONO,
+		DIST,
+		COL_BY,
+		ISFOUND,
+		ACTION,
+		POSREL,
+		KEYBOARD_INPUT,
+		SPACE,
+		CONTACT,
+		ALPHA,
+		BETA,
+		GAMMA,
+		INHAND,
+		HITFORCE,
+		CAMERA,
+		RAND,
+		FALLING,
+		ROOM,
+		LIFE,
+		OBJECT,
+		ROOMY,
+		TEST_ZV_END_ANIM,
+		MUSIC,
+		C_VAR,
+		STAGE,
+		THROW,
+		MATRIX,
+		HARD_MAT,
+		PROTECT,
+		END_WAY,
+		DIV_BY_2,
+		UNKNOWN
+	};
+}
+
+namespace LifeEnum {
+	enum LifeEnum
+	{
+		DO_MOVE,
+		ANIM_ONCE,
+		ANIM_ALL_ONCE,
+		BODY,
+		IF_EGAL,
+		IF_DIFFERENT,
+		IF_SUP_EGAL,
+		IF_SUP,
+		IF_INF_EGAL,
+		IF_INF,
+		GOTO,
+		RETURN,
+		ENDLIFE,
+		ANIM_REPEAT,
+		ANIM_MOVE,
+		TRACKMODE,
+		HIT,
+		MESSAGE,
+		MESSAGE_VALUE,
+		VAR,
+		INC,
+		DEC,
+		ADD,
+		SUB,
+		LIFE_MODE,
+		SWITCH,
+		CASE,
+		CAMERA,
+		START_CHRONO,
+		MULTI_CASE,
+		FOUND,
+		LIFE,
+		DELETE,
+		TAKE,
+		IN_HAND,
+		READ,
+		ANIM_SOUND,
+		SPECIAL,
+		DO_REAL_ZV,
+		SOUND,
+		FLAGS,
+		GAME_OVER,
+		MANUAL_ROT,
+		RND_FREQ,
+		MUSIC,
+		SET_BETA,
+		DO_ROT_ZV,
+		CHANGEROOM,
+		FOUND_NAME,
+		FOUND_FLAG,
+		FOUND_LIFE,
+		CAMERA_TARGET,
+		DROP,
+		FIRE,
+		TEST_COL,
+		FOUND_BODY,
+		SET_ALPHA,
+		STOP_BETA,
+		DO_MAX_ZV,
+		PUT,
+		C_VAR,
+		DO_NORMAL_ZV,
+		DO_CARRE_ZV,
+		SOUND_THEN,
+		LIGHT,
+		SHAKING,
+		ALLOW_INVENTORY,
+		FOUND_WEIGHT,
+		UP_COOR_Y,
+		SPEED,
+		PUT_AT,
+		DEF_ZV,
+		HIT_OBJECT,
+		GET_HARD_CLIP,
+		ANGLE,
+		REP_SOUND,
+		THROW,
+		WATER,
+		PICTURE,
+		STOP_SOUND,
+		NEXT_MUSIC,
+		FADE_MUSIC,
+		STOP_HIT_OBJECT,
+		COPY_ANGLE,
+		END_SEQUENCE,
+		SOUND_THEN_REPEAT,
+		WAIT_GAME_OVER,
+		GET_MATRICE,
+		STAGE_LIFE,
+		CONTINUE_TRACK,
+		ANIM_RESET,
+		RESET_MOVE_MANUAL,
+		PLUIE,
+		ANIM_HYBRIDE_ONCE,
+		ANIM_HYBRIDE_REPEAT,
+		CALL_INVENTORY,
+		BODY_RESET,
+		DEL_INVENTORY,
+		SET_INVENTORY,
+		PLAY_SEQUENCE,
+		_2D_ANIM_SOUND,
+		SET_GROUND,
+		PROTECT,
+		DEF_ABS_ZV,
+		DEF_SEQUENCE_SOUND,
+		READ_ON_PICTURE,
+		FIRE_UP_DOWN,
+		DO_ROT_CLUT,
+		STOP_CLUT,
+		IF_IN,
+		IF_OUT,
+		UNKNOWN_1,
+		SET_VOLUME_SOUND,
+		UNKNOWN_2,
+		UNKNOWN_3,
+		FADE_IN_MUSIC,
+		SET_MUSIC_VOLUME,
+		MUSIC_AND_LOOP,
+		MUSIC_A,
+		MUSIC_B,
+		START_FADE_IN_MUSIC,
+		START_FADE_IN_MUSIC_THEN,
+		START_FADE_IN_MUSIC_LOOP,
+		FADE_OUT_MUSIC_STOP,
+		MUSIC_ALTER_TEMPO,
+		REP_SOUND_N_TIME,
+		UNKNOWN_4,
+		DEBUG,
+		UNKNOWN_5,
+		UNKNOWN_6,
+		CASE_DEFAULT = 0x10000
+	};
+}
+
+typedef struct LifeExpr
+{
+	EvalEnum::EvalEnum Type = EvalEnum::CONST;
+	s16 Actor = -1;
+	s16 constVal = 0;
+	vector<LifeExpr> arguments;
+} EvalFunction;
+
+typedef struct LifeInstruction
+{
+	LifeEnum::LifeEnum Type;
+	vector<LifeExpr> arguments;
+	s16 Actor = -1;
+	int Goto = -1;
+	int Position = -1; //used to map gotos to a given instruction
+	int Size = -1; //size in bytes of instruction
+} LifeInstruction;
+
+enum parseArgType {
+	lifeConst,
+	lifeExpr,
+	lifeGoto
+};
+
+struct parseLifeFunction {
+	EvalEnum::EvalEnum Type;
+	vector<parseArgType> arguments;
+};
+
+struct parseLifeInstruction {
+	LifeEnum::LifeEnum Type;
+	vector<parseArgType> arguments;
+};
