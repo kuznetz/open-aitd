bb2a48c977de06e7354aa4f0c5c89fb764a9dc14
Georgiy Kuznecov <georgiy.kuznecov@gmail.com>
2025-04-20 16:43:56 +0300

    Inventory interface

--- src/engine/controllers/inventory_screen.h
+++ src/engine/controllers/inventory_screen.h
@@ -14,7 +14,9 @@ namespace openAITD {
 		World* world;
 		Resources* resources;
 		
+		vector<int> curActions;
 		int curItemIdx = 0;
+		int curActionIdx = 0;
 		int itemsOffs = 0;
 		bool firstFrame = true;
 		bool selAction = false;
@@ -35,6 +37,20 @@ namespace openAITD {
 			exit = false;
 			selAction = false;
 			firstFrame = true;
+			reloadActions();
+		}
+
+		void reloadActions() {
+			curActionIdx = 0;
+			curActions.clear();
+			if (!world->inventory.size()) return;
+			auto& gobj = *world->inventory[curItemIdx];
+			for (int i = 0; i < 11; i++) {
+				if (gobj.invItem.flags & (1 << i))
+				{
+					curActions.push_back(i + 23);
+				}
+			}
 		}
 
 		void drawLines() {
@@ -42,6 +58,33 @@ namespace openAITD {
 			auto& h = resources->config.screenH;
 			DrawLineEx({ 0 ,(float)(h / 2) }, { (float)w ,(float)(h / 2) }, 2, GRAY);
 			DrawLineEx({ (float)(w / 2) ,(float)(h / 2) }, { (float)(w / 2) ,(float)h }, 2, GRAY);
+		}
+
+		void drawActions() {
+			auto& f = resources->mainFont;
+			float h2 = (resources->config.screenH / 2);
+			float YOffs = (h2 - (f.baseSize * curActions.size())) / 2;
+			raylib::Rectangle r = { resources->config.screenW / 2, h2 + YOffs, resources->config.screenW / 2, 0 };
+			for (int i = 0; i < 5; i++) {
+				if (i >= curActions.size()) break;
+				auto& name = resources->texts[curActions[i]];
+				Color c = GRAY;
+				if (selAction) {
+					c = (i == curActionIdx) ? GOLD : WHITE;
+				}
+				resources->drawCentered(name.c_str(), r, c);
+				r.y += f.baseSize;
+			}
+		}
+
+		void nextAction() {
+			if (curActionIdx >= curActions.size() - 1) return;
+			curActionIdx++;
+		}
+
+		void prevAction() {
+			if (curActionIdx <= 0) return;
+			curActionIdx--;
 		}
 
 		void drawItems() {
@@ -52,7 +95,10 @@ namespace openAITD {
 				if (id >= world->inventory.size()) break;
 				auto& gobj = *world->inventory[id];
 				auto& name = resources->texts[gobj.invItem.nameId];
-				Color c = (id == curItemIdx) ? GOLD : WHITE;
+				Color c = GRAY;
+				if (!selAction) {
+					c = (id == curItemIdx) ? GOLD : WHITE;
+				}
 				resources->drawCentered(name.c_str(), r, c);
 				r.y += f.baseSize;
 			}
@@ -62,24 +108,14 @@ namespace openAITD {
 			if (curItemIdx >= world->inventory.size() - 1) return;
 			curItemIdx++;
 			if (curItemIdx >= (itemsOffs + 4)) itemsOffs++;
+			reloadActions();
 		}
 
 		void prevItem() {
 			if (curItemIdx <= 0) return;
 			curItemIdx--;
 			if (curItemIdx-1 < itemsOffs && itemsOffs > 0) itemsOffs--;
-		}
-
-		void drawActions() {
-			auto& f = resources->mainFont;
-			raylib::Rectangle r = { 0, resources->config.screenH * 0.05, resources->config.screenW, 0 };
-			for (int i = 0; i < 5; i++) {
-				if (i >= world->inventory.size()) break;
-				auto& gobj = *world->inventory[i];
-				auto& name = resources->texts[gobj.invItem.nameId];
-				resources->drawCentered(name.c_str(), r, WHITE);
-				r.y += f.baseSize;
-			}
+			reloadActions();
 		}
 
 		void processKeys() {
@@ -105,7 +141,7 @@ namespace openAITD {
 					prevItem();
 				}
 				else {
-
+					prevAction();
 				}
 			}
 			if (IsKeyPressed(KEY_DOWN)) {
@@ -113,7 +149,7 @@ namespace openAITD {
 					nextItem();
 				}
 				else {
-
+					nextAction();
 				}
 			}
 		}
@@ -130,6 +166,7 @@ namespace openAITD {
 			ClearBackground(BLACK);
 			drawLines();
 			drawItems();
+			drawActions();
 			EndDrawing();
 			firstFrame = false;
 		}
