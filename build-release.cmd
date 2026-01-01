5bf1ef9222af5f5341eb657dbcfe4026abf02c0f
Georgiy Kuznetsov <georgiy.kuznecov@gmail.com>
2026-01-01 20:27:35 +0300

    Worked build and install with libs from submodles

--- build-release.cmd
+++ build-release.cmd
@@ -1,3 +1,4 @@
 mkdir build-release
 cmake -S . -B build-release -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release
 cmake --build build-release --config Release --target ALL_BUILD --parallel
+cmake --install build-release --prefix "./dist"
